a72059fcccd5cc9bb3657d374cef37e0bd591ecc
Ranjit Bishwokarma <ranjitbishwokarma99@gmail.com>
2024-03-24 21:13:18 +0545

    completed

--- markup/src/js/components/swiper.js
+++ markup/src/js/components/swiper.js
@@ -1,16 +1,15 @@
 import Swiper from "swiper/swiper-bundle.js";
 
 export default function initSwiper() {
-    var newsCarousel = new Swiper(".categories-carousel", {
+    var productSlider = new Swiper(".product-slider", {
         slidesPerView: 1,
         spaceBetween: 25,
         speed: 450,
         loop: true,
         breakpoints: {
             768: {
-                spaceBetween: 0,
-                slidesPerView: 4,
-                loop: false,
+                spaceBetween: 25,
+                slidesPerView: 3,
             },
         },
         pagination: {
@@ -19,93 +18,20 @@ export default function initSwiper() {
         },
     });
 
-
-    var newsCarousel = new Swiper(".stores-carousel", {
+    var testimonialSlider = new Swiper(".testimonial-slider", {
         slidesPerView: 1,
         spaceBetween: 25,
         speed: 450,
         loop: true,
         breakpoints: {
             768: {
-                spaceBetween: 40,
-                slidesPerView: 3,
-                loop: false,
-            },
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        },
-    });
-
-
-    var newsCarousel = new Swiper(".news-carousel", {
-        slidesPerView: 1,
-        spaceBetween: 25,
-        speed: 450,
-        loop: true,
-        breakpoints: {
-            768: {
-                spaceBetween: 60,
-                slidesPerView: 3,
-                loop: false,
-            },
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
-
-    var testimonialsCarousel = new Swiper(".testimonials-carousel", {
-        slidesPerView: 1,
-        spaceBetween: 25,
-        speed: 450,
-        loop: true,
-        breakpoints: {
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
-
-    var newsCarousel = new Swiper(".article-carousel", {
-        slidesPerView: 1,
-        spaceBetween: 25,
-        speed: 450,
-        loop: true,
-        breakpoints: {
-            768: {
-                spaceBetween: 40,
-                slidesPerView: 3,
-                loop: false,
-            },
-            
-            1200: {
-                spaceBetween: 60,
-                slidesPerView: 3,
-            },
-            1600: {
-                spaceBetween: 93,
+                spaceBetween: 25,
                 slidesPerView: 3,
             },
         },
         pagination: {
             el: ".swiper-pagination",
             clickable: true
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
         },
     });
 
